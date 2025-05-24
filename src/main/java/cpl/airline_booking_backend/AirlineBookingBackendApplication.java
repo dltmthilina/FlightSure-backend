0a4e0f96dc9b8b7f1d612be67958ef4340b5fee8
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-05-24 10:21:17 +0530

    initial project structure

---
+++ src/main/java/cpl/airline_booking_backend/AirlineBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package cpl.airline_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cpl/airline_booking_backend/AirlineBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cpl.airline_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
